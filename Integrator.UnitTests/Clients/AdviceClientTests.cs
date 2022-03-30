d58015c1176a478f7f55edffd45a4a5de55284b2
Kako Ferrare <kakoferrare87@gmail.com>
2022-03-30 10:30:49 -0300

    tests: add integrator tests

---
+++ Integrator.UnitTests/Clients/AdviceClientTests.cs
@@ -0,0 +1,39 @@
+﻿using IntegratorHttpClient;
+using Moq;
+using System;
+using System.Net.Http;
+using Xunit;
+
+namespace Integrator.UnitTests.Clients
+{
+    public class AdviceClientTests
+    {
+        private readonly AdviceClient _sut;
+        private readonly MockRepository _mockRepository;
+        private readonly Mock<HttpClient> _mockClient;
+
+        public AdviceClientTests()
+        {
+            _mockRepository = new MockRepository(MockBehavior.Loose);
+            _mockClient = _mockRepository.Create<HttpClient>();
+            _sut = new AdviceClient(_mockClient.Object);
+        }
+
+        [Fact]
+        public async void GetAsync_ReturnAdviceToClient()
+        {
+            // Arrange
+            /*string stringResponse = JsonConvert.SerializeObject(new AdviceDto()
+            {
+                Slip = new SlipDto() { Advice = "asdasd", Id = 1 }
+            });*/
+            _mockClient.Object.BaseAddress = new Uri("https://api.adviceslip.com/");
+
+            // Act
+            var result = await _sut.Get("advice");
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ Integrator.UnitTests/Clients/ChukNorrisClientTests.cs
@@ -0,0 +1,36 @@
+﻿using IntegratorHttpClient;
+using Moq;
+using System;
+using System.Net.Http;
+using Xunit;
+
+namespace Integrator.UnitTests.Clients
+{
+    public class ChukNorrisClientTests
+    {
+        private readonly ChukNorrisClient _sut;
+        private readonly MockRepository _mockRepository;
+        private readonly Mock<HttpClient> _mockClient;
+
+
+        public ChukNorrisClientTests()
+        {
+            _mockRepository = new MockRepository(MockBehavior.Loose);
+            _mockClient = _mockRepository.Create<HttpClient>();
+            _sut = new ChukNorrisClient(_mockClient.Object);
+        }
+
+        [Fact]
+        public async void GetAsync_ReturnChuckNorrisToClient()
+        {
+            // Arrange
+            _mockClient.Object.BaseAddress = new Uri("https://api.chucknorris.io/");
+
+            // Act
+            var result = await _sut.Get("jokes/random");
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
